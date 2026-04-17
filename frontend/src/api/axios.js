935e0e6a2ee7c5d645868e95008d635477f59177
Fahad Pro <perodeveloper855@gmail.com>
2026-04-17 17:10:10 +0500

    fix bug in api call in axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'https://haul-ecommerce.onrender.com',
+  baseURL: 'https://haul-ecommerce.onrender.com/api',
 })
 
 API.interceptors.request.use((req) => {
